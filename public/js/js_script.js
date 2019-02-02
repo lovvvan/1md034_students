0f08dd2c458a3059c174111977c410c4d958e634
lovvvan <lovisa.hedman93@gmail.com>
2019-02-02 14:01:08 +0100

    JS dynamic menu

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -2,23 +2,65 @@
   'use strict';
 
   // (String, Int, Bool) ->
-  function MenuItem(name, kCal, allergens) {
+  function MenuItem(name, img, kCal, gluten) {
     this.name = name;
+    this.img = img;
+    //this.info = info;
     this.kCal = kCal;
-    this.allergens = allergens;
+    this.gluten = gluten;
     this.getInfo = function() {
       return this.name + ' ' + this.kCal;
     }
   }
 
-  let burger = new MenuItem("Pumpkin Burger", 230, true);
-  console.log(burger);
-  console.log(burger.name);
-  console.log(burger.getInfo());
+  let beenBurger         = new MenuItem("Black Been burger", './img/blackbeenBurger.jpg', 350, false);
+  let fakeHalloumiBurger = new MenuItem("Fake-Halloumi Burger", './img/halloumiBurger.jpg', 400, true);
+  let pumBurger          = new MenuItem("Pumpkin Burger", './img/pumpkinBurger.jpg', 200, false);
 
-  let beenBurger = new MenuItem("Been burger", 400, true);
-  console.log(burger);
-  console.log(burger.name);
-  console.log(burger.getInfo());
+  var wrapper = document.getElementById('wrapper');
+
+  var burgers    = [beenBurger, fakeHalloumiBurger, pumBurger];
+  var boxLetters = ['a', 'b', 'c'];
+
+  var index = 0;
+  var i;
+  for (i = 0; i < burgers.length; i++) {
+    var box = document.createElement('div');
+    box.setAttribute('class', 'box');
+    box.classList.add(boxLetters[index]);
+    wrapper.appendChild(box);
+
+    var h = document.createElement('h4');
+    var headline = document.createTextNode(burgers[i].name);
+    h.appendChild(headline);
+    box.appendChild(h);
+
+    var image = document.createElement('img');
+    image.setAttribute('class', 'burger');
+    image.setAttribute('src', burgers[i].img);
+    image.setAttribute('alt', burgers[i].name);
+    image.setAttribute('title', burgers[i].name);
+    image.setAttribute('width', 200);
+    image.setAttribute('height', 200);
+    box.appendChild(image);
+
+    var listDiv = document.createElement('div');
+    listDiv.setAttribute('class', 'ingredients');
+    box.appendChild(listDiv);
+
+    var ul = document.createElement('ul');
+    listDiv.appendChild(ul);
+    var li1 = document.createElement('li');
+    var li1Txt = document.createTextNode(burgers[i].kCal + ' kCal');
+    li1.appendChild(li1Txt);
+    ul.appendChild(li1);
+    if (burgers[i].gluten == true) {
+      var li2 = document.createElement('li');
+      li2.innerHTML = 'Contains <span class="allergy">Gluten</span>';
+      ul.appendChild(li2);
+    }
+    index += 1;
+  }
+
 
 // }());
